d2818e365e66a35fa97fdd5d6c5d06243577f707
gcoter <guillaume.coter@gmail.com>
2021-02-28 15:22:54 +0100

    Add a CLI to fetch hot submissions of one subreddit and insert the comments and authors in the DB

---
+++ mbti_type_from_text/cli/fetch_hot_comments.py
@@ -0,0 +1,65 @@
+import argparse
+import logging
+
+from praw import Reddit
+
+from mbti_type_from_text.db_utils import create_connection, insert_or_update_submission
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(name="fetch_hot_comments")
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--db_path",
+        type=str,
+        required=True,
+        help="Path to the database file"
+    )
+    parser.add_argument(
+        "--subreddit",
+        type=str,
+        required=True,
+        help="Name of the subreddit from which hot comments will be fetched"
+    )
+    parser.add_argument(
+        "--submission_flairs",
+        nargs="+",
+        help="List of flairs to select",
+        required=True
+    )
+    parser.add_argument(
+        "--n_hot",
+        type=int,
+        required=True,
+        help="Number of hot submissions to fetch"
+    )
+    parser.add_argument(
+        "--reddit_client_id",
+        type=str,
+        required=True,
+        help="Client ID to use with Reddit's API"
+    )
+    parser.add_argument(
+        "--reddit_client_secret",
+        type=str,
+        required=True,
+        help="Client secret to use with Reddit's API"
+    )
+    args = parser.parse_args()
+
+    logger.info("Create connection to '{}'".format(args.db_path))
+    db_connection = create_connection(db_path=args.db_path)
+
+    logger.info("Connect to Reddit API")
+    reddit = Reddit(
+        user_agent="mbti-type-from-text:v0.0.1 (by /u/gcoter)",
+        client_id=args.reddit_client_id,
+        client_secret=args.reddit_client_secret
+    )
+
+    logger.info("Start iterating over submissions")
+    for n, submission in enumerate(reddit.subreddit(args.subreddit).hot(limit=args.n_hot)):
+        if submission.is_self and submission.num_comments > 0 and submission.link_flair_text in args.submission_flairs:
+            logger.info("Handle submission '{}' ({}/{})".format(submission.title, n + 1, args.n_hot))
+            insert_or_update_submission(submission=submission, db_connection=db_connection)
--- mbti_type_from_text/db_utils.py
+++ mbti_type_from_text/db_utils.py
@@ -1,5 +1,8 @@
 import logging
 import sqlite3
+from datetime import datetime
+
+from praw.models import MoreComments
 
 logger = logging.getLogger(name="db_utils")
 
@@ -20,6 +23,7 @@ def execute_query(query, db_connection):
     logger.debug("Execute query:")
     logger.debug(query)
     cursor.execute(query)
+    db_connection.commit()
 
 
 def insert_or_update_user(user_id, user_name, user_flair_text, db_connection):
@@ -33,19 +37,27 @@ def insert_or_update_user(user_id, user_name, user_flair_text, db_connection):
     execute_query(query=query, db_connection=db_connection)
 
 
+def format_date_for_db(created_utc):
+    # created_utc is UNIX
+    # SQLite expects dates in format 'YYYY-MM-DD HH:MM:SS'
+    dt = datetime.fromtimestamp(created_utc)
+    return dt.strftime("%Y-%m-%d %H:%M:%S")
+
+
 def insert_or_update_comment(
     comment_id,
     comment_user_id,
     comment_parent_id,
     comment_title,
     comment_content,
-    comment_created_datetime,
+    comment_created_utc,
     comment_upvotes,
+    subreddit_display_name,
     db_connection,
 ):
     query = """
-    INSERT INTO Comments (id, user_id, parent_comment_id, title, content, created_datetime, upvotes) 
-    VALUES ('{comment_id}', '{comment_user_id}', '{comment_parent_id}', '{comment_title}', '{comment_content}', '{comment_created_datetime}', {comment_upvotes}) 
+    INSERT INTO Comments (id, user_id, parent_comment_id, title, content, created_datetime, upvotes, subreddit) 
+    VALUES ('{comment_id}', '{comment_user_id}', '{comment_parent_id}', '{comment_title}', '{comment_content}', '{comment_created_datetime}', {comment_upvotes}, '{subreddit_display_name}') 
     ON CONFLICT (id) 
     DO UPDATE SET title = '{comment_title}', content = '{comment_content}', created_datetime = '{comment_created_datetime}', upvotes = {comment_upvotes} 
     WHERE id = '{comment_id}'
@@ -53,46 +65,68 @@ def insert_or_update_comment(
         comment_id=comment_id,
         comment_user_id=comment_user_id,
         comment_parent_id=comment_parent_id,
-        comment_title=comment_title,
-        comment_content=comment_content,
-        comment_created_datetime=comment_created_datetime,
-        comment_upvotes=comment_upvotes
+        comment_title=comment_title.replace("'", "''"),
+        comment_content=comment_content.replace("'", "''"),
+        comment_created_datetime=format_date_for_db(created_utc=comment_created_utc),
+        comment_upvotes=comment_upvotes,
+        subreddit_display_name=subreddit_display_name
     )
     execute_query(query=query, db_connection=db_connection)
 
 
 def insert_or_update_comment_forest(comments, parent_id, db_connection):
     for comment in comments:
-        insert_or_update_comment(
-            comment_id=comment.id,
-            comment_user_id=comment.author.id,
-            comment_parent_id=parent_id,
-            comment_title="",
-            comment_content=comment.body,
-            comment_created_datetime=comment.created_utc,
-            comment_upvotes=comment.score,
-            db_connection=db_connection,
-        )
+        if isinstance(comment, MoreComments):
+            insert_or_update_comment_forest(comments=comment.comments(), parent_id=parent_id, db_connection=db_connection)
+        else:
+            if comment.author is not None and hasattr(comment.author, "id"):
+                insert_or_update_user(
+                    user_id=comment.author.id,
+                    user_name=comment.author.name,
+                    user_flair_text=comment.author_flair_text if comment.author_flair_text is not None else "",
+                    db_connection=db_connection
+                )
+                insert_or_update_comment(
+                    comment_id=comment.id,
+                    comment_user_id=comment.author.id,
+                    comment_parent_id=parent_id,
+                    comment_title="",
+                    comment_content=comment.body,
+                    comment_created_utc=comment.created_utc,
+                    comment_upvotes=comment.score,
+                    subreddit_display_name=comment.subreddit.display_name,
+                    db_connection=db_connection,
+                )
+            else:
+                logger.warning("Comment without author (id='{}', parent_id='{}')".format(comment.id, parent_id))
+            assert hasattr(comment, "id") and comment.id is not None
+            assert hasattr(comment, "replies") and comment.replies is not None
+            insert_or_update_comment_forest(comments=comment.replies, parent_id=comment.id, db_connection=db_connection)
 
 
 def insert_or_update_submission(submission, db_connection):
-    logger.info("Handle submission '{}' (id='{}')".format(submission.title, submission.id))
-    insert_or_update_user(
-        user_id=submission.author.id,
-        user_name=submission.author.name,
-        user_flair_text=submission.author_flair_text,
-        db_connection=db_connection
-    )
-    insert_or_update_comment(
-        comment_id=submission.id,
-        comment_user_id=submission.author.id,
-        comment_parent_id="",
-        comment_title=submission.title,
-        comment_content=submission.selftext,
-        comment_created_datetime=submission.created_utc,
-        comment_upvotes=submission.score,
-        db_connection=db_connection,
-    )
+    if submission.author is not None and hasattr(submission.author, "id"):
+        insert_or_update_user(
+            user_id=submission.author.id,
+            user_name=submission.author.name,
+            user_flair_text=submission.author_flair_text if submission.author_flair_text is not None else "",
+            db_connection=db_connection
+        )
+        insert_or_update_comment(
+            comment_id=submission.id,
+            comment_user_id=submission.author.id,
+            comment_parent_id="",
+            comment_title=submission.title,
+            comment_content=submission.selftext,
+            comment_created_utc=submission.created_utc,
+            comment_upvotes=submission.score,
+            subreddit_display_name=submission.subreddit.display_name,
+            db_connection=db_connection,
+        )
+    else:
+        logger.warning("Submission without author (id='{}')".format(submission.id))
+    assert hasattr(submission, "id") and submission.id is not None
+    assert hasattr(submission, "comments") and submission.comments is not None
     insert_or_update_comment_forest(comments=submission.comments, parent_id=submission.id, db_connection=db_connection)
 
 
